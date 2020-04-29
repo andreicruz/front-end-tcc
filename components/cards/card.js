7f1fa16535be1268410fca0cecb6ed1eaf3dc1fb
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2020-04-29 20:59:39 -0300

    final changes in card component

--- components/cards/card.js
+++ components/cards/card.js
@@ -3,24 +3,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { styles } from './cardStyle';
 import { icons } from '../../utils/icons';
 import MyAppText from '../myAppText/text';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { globalFonts } from '../../utils/globalStyles';
 
 export default function Card() {
     return (
-        <View style={styles.card}>
-            <View style={[styles.item, {flex: 1, flexDirection: "row"}]}>
-                <View style={{width: 40}}>
-                    <FontAwesomeIcon color={globalFonts.blueText.color} icon={ icons.iconFaCamera } size={ 24 } />
+        <View style={ styles.card }>
+            <View style={{ display: "flex", flexDirection: "row" }}>
+                <View style={[styles.item, { flex: 1, flexDirection: "row", paddingLeft: 40 }]}>
+                    <View>
+                        <FontAwesomeIcon color={globalFonts.blueText.color} icon={ icons.iconFaCamera } size={ 36 } />
+                    </View>
+                    <View style={{ marginTop: 5, marginLeft: 15 }}>
+                        <MyAppText text="Usar a câmera" fontSize={22} textColor={globalFonts.blueText.color}/>
+                    </View>
                 </View>
-                <View style={{marginTop: 5}}>
-                    <MyAppText text="Usar a câmera" fontSize={22} textColor={globalFonts.blueText.color}/>
+                <View style={[styles.cornerBorder, {flex: 0.1}]}>
+                    <View style={[styles.item, { marginTop: 10, paddingLeft: 3 }]}>
+                        <FontAwesomeIcon color={ globalFonts.whiteText.color } icon={ icons.iconFaArrowLeft } size={ 20 } />
+                    </View>
                 </View>
-            </View>
-            <View style={styles.cornerBorder}>
-                <Text style={styles.iconBorder}>
-                    <FontAwesomeIcon color={ globalFonts.whiteText.color } icon={ icons.iconFaArrowLeft } size={ 12 } />
-                </Text>
             </View>
         </View>
     );
--- components/cards/cardStyle.js
+++ components/cards/cardStyle.js
@@ -5,32 +5,15 @@ export const styles = StyleSheet.create({
         borderColor: '#d6d6d6',
         borderWidth: 1,
         borderRadius: 6,
-        display: "flex",
-        flexDirection: "row",
-        height: 100,
+
     },
     item: {
-        flex: 1,
-        paddingTop: 20,
-        paddingBottom: 20,
-        paddingLeft: 40,
-        // justifyContent: 'center',
-        alignItems: 'center',
+        paddingBottom: 40,
+        paddingTop: 40,
     },
     cornerBorder: {
         backgroundColor: "#1b75bc",
         borderBottomRightRadius: 4,
         borderTopRightRadius: 4,
-        position: 'relative',
-        width: 20,
     },
-    iconBorder: {
-        left: 5,
-        position: 'absolute',
-        // transform: [
-        //     { translateX: 50 },
-        //     { translateY:  50 },
-        // ],
-        top: 50
-    }
 });
